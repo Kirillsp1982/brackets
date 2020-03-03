5c569ecba457d982a93411309b2c7598b42c7def
Kirillsp1982 <kirillsp1982@gmail.com>
2020-03-03 10:01:13 +0300

    feat: add first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   for (let i = 0; i < bracketsConfig.length; i++) {
+	  let bracketLeft = bracketsConfig[i][0];
+	  let bracketRight = bracketsConfig[i][1];
+	  let count = 0;
+	  
+	  if (bracketLeft === bracketRight) {
+		  for (let k = 0; k < str.length; k++) {
+			  if (bracketLeft === str[k]) count++;
+	      }
+		  if (count % 2  !== 0) return false;
+	  }else {
+		  for (let j = 0; j < str.length; j++) {
+		    if (bracketLeft === str[j]) count++;
+			if (bracketLeft === str[j]) count--;
+			if (count < 0) return false;
+	      }
+		  if (count !== 0) return false;
+	  } 	    
+  }
+  return true;
 }
