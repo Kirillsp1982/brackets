9e3a874cc729a3b4fbf542b06738c913c176eadd
Kirillsp1982 <kirillsp1982@gmail.com>
2020-12-29 14:45:31 +0300

    feat: add new solution

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-   for (let i = 0; i < bracketsConfig.length; i++) {
-	  let bracketLeft = bracketsConfig[i][0];
-	  let bracketRight = bracketsConfig[i][1];
-	  let count = 0;
-	  
-	  if (bracketLeft === bracketRight) {
-		  for (let k = 0; k < str.length; k++) {
-			  if (bracketLeft === str[k]) count++;
-	      }
-		  if (count % 2  !== 0) return false;
-	  }else {
-		  for (let j = 0; j < str.length; j++) {
-		    if (bracketLeft === str[j]) count++;
-			if (bracketRight === str[j]) count--;
-			if (count < 0) return false;
-	      }
-		  if (count !== 0) return false;
-	  } 	    
+const stack = [],
+        leftBrackets = bracketsConfig.filter(item => item[0] !== item[1]).map(item => item[0]),
+		rightBrackets = bracketsConfig.filter(item => item[0] !== item[1]).map(item => item[1]),
+		sameBrackets = bracketsConfig.filter(item => item[0] === item[1]).map(item => item[0]);
+
+  for (let i = 0; i < str.length; i++) {
+	  if ( leftBrackets.indexOf(str[i]) >= 0 ) {
+		  stack.push(str[i]);
+	  } else if ( rightBrackets.indexOf(str[i]) >= 0 ) {
+		  if (stack.length > 0 && rightBrackets.indexOf(str[i]) === leftBrackets.indexOf(stack[stack.length - 1])) {
+		    stack.pop();
+		  } else return false;
+	  } else if ( sameBrackets.indexOf(str[i]) >= 0 ) {
+		  if ( stack.indexOf(str[i]) < 0 ) {
+			  stack.push(str[i]);
+		  }else if ( stack.indexOf(str[i]) >= 0 && sameBrackets.indexOf(stack[stack.length - 1]) === sameBrackets.indexOf(str[i])) {
+			  
+			  stack.pop();
+		  }else return false;
+	  }
   }
-  return true;
+  return stack.length === 0 ? true : false;
 }
